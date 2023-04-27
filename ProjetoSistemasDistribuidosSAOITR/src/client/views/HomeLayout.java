d8c193befa7bd061a01e94dc62d8d6af395cbf7e
a2258943 <a2258943@pg-lab8-wx6.campus.pg.utfpr.edu.br>
2023-04-27 16:39:34 -0300

    Create HomeLayout first project delivery

---
+++ ProjetoSistemasDistribuidosSAOITR/src/client/views/HomeLayout.java
@@ -0,0 +1,54 @@
+package client.views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
+public class HomeLayout extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomeLayout frame = new HomeLayout();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HomeLayout() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 498, 350);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Página Inicial");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(188, 11, 107, 14);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnLogout = new JButton("Logout");
+		btnLogout.setBounds(206, 277, 89, 23);
+		contentPane.add(btnLogout);
+	}
+
+}
