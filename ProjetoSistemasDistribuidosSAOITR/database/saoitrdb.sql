359e690c951d803ddd27ddc321b1c7f070cdb34e
GabrielSauter <gabriel.andrade.sauter@gmail.com>
2023-05-25 10:09:35 -0300

    Update database

--- ProjetoSistemasDistribuidosSAOITR/database/saoitrdb.sql
+++ ProjetoSistemasDistribuidosSAOITR/database/saoitrdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Tempo de geração: 23/05/2023 às 22:23
+-- Tempo de geração: 25/05/2023 às 15:06
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -43,7 +43,14 @@ CREATE TABLE `incident` (
 INSERT INTO `incident` (`id_incident`, `id_user`, `date`, `highway`, `km`, `incident_type`) VALUES
 (12, 42, '2023-05-22 22:21:43', 'AB-123', 123, 5),
 (15, 44, '2023-05-23 15:00:00', 'BR-222', 97, 7),
-(16, 44, '2023-05-23 15:30:00', 'BR-222', 67, 10);
+(16, 44, '2023-05-23 15:30:00', 'BR-222', 67, 10),
+(19, 48, '2023-05-23 22:26:42', 'BR-777', 50, 1),
+(20, 48, '2023-05-23 22:27:06', 'BR-666', 40, 6),
+(21, 45, '2023-05-23 22:54:22', 'BR-123', 123, 4),
+(22, 44, '2023-05-23 23:00:00', 'BR-222', 120, 9),
+(27, 51, '2023-05-24 10:50:48', 'BR-333', 33, 3),
+(28, 51, '2023-05-24 10:46:12', 'BR-555', 5, 5),
+(29, 51, '2023-05-24 22:33:48', 'BR-123', 55, 5);
 
 -- --------------------------------------------------------
 
@@ -66,7 +73,11 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `token`) VALUES
 (41, 'Gabriel Oliveira', 'gabriel@hotmail.com', '}rl}jl:;<', ''),
 (42, 'abcde', 'abcdefgh@gmail.com', 'jklmn:;<=', ''),
-(44, 'Gabriel Login', 'gabriellogin@hotmail.com', '~sm~km;<=>', '');
+(44, 'Gabriel', 'gabriel2@gmail.com', '}rl}jl:;<', ''),
+(45, 'almeida edu', 'almeida1@gmail.com', 'kvwosnk;<=', ''),
+(46, 'gabriel sauter123', 'gabrielSauter123@gmail.com', 'qkl|sov;<=', ''),
+(48, 'Lukas Kenji', 'LukasKenji@gmail.com', 't}si{9:;', ''),
+(51, 'almeida bento', 'almeidabento@gmail.com', 'kvwosnk;<=', '');
 
 --
 -- Índices para tabelas despejadas
@@ -93,13 +104,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de tabela `incident`
 --
 ALTER TABLE `incident`
-  MODIFY `id_incident` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_incident` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de tabela `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- Restrições para tabelas despejadas
